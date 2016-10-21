0bde7e4b3f62498b2168238186c159f96c259f0c
LiChunM <1445266146@qq.com>
2016-10-21 19:54:18 +0800

    ok

--- kongge.py
+++ kongge.py
@@ -1,4 +1,6 @@
 import os
+import xlrd
+import subprocess
 
 def rename():
     path=r"F:\laopo\1"
@@ -14,3 +16,21 @@ def rename():
         os.rename(Olddir,Newdir);
 
 rename();
+
+namepath = r"C:\profiles\test.exe"
+p = subprocess.Popen(namepath, stdin = subprocess.PIPE,stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell = False)
+p.stdin.write("344.95\n") 
+p.stdin.write("yizhi\n")
+p.stdin.write("F:\\laopo\\yizhi.txt\n")
+p.stdin.write("766.025\n")
+p.stdin.write("1-1\n")
+p.stdin.write("F:\\laopo\\1\\1.txt\n")
+fname = r"F:\laopo\1.xls"
+bk = xlrd.open_workbook(fname)
+table=bk.sheets()[0]
+cell=table.cell(1,1).value
+p.stdin.write(str(cell)+"\n")
+p.stdin.write("C:\\profiles\\1-3.TXT\n")
+print p.stdout.read() 
+
+
